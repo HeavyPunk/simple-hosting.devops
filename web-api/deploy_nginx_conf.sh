565ab981e7acb2e7234b112ed09a4c90b5f866bf
HeavyPunk <kirillkuchma8@gmail.com>
2023-06-07 17:14:10 +0500

    Nginx config for all requests

--- web-api/deploy_nginx_conf.sh
+++ web-api/deploy_nginx_conf.sh
@@ -1,4 +1,6 @@
 export ASSETS_PATH=/home/kirieshki/assets
+export MACHINE_IP=158.160.100.38
 
-envsubst < nginx.conf.template > nginx.conf
-scp -P 22 -i ~/.ssh/ya_cloud nginx.conf kirieshki@158.160.45.53:/home/kirieshki/ 
+envsubst < nginx.template.conf > nginx.conf
+scp -P 22 -i ~/.ssh/ya_cloud nginx.conf kirieshki@$MACHINE_IP:/home/kirieshki/ 
+ssh -i ~/.ssh/ya_cloud kirieshki@$MACHINE_IP 'sudo systemctl restart nginx' 
