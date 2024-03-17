5c5e44caf21e8e967e765f769c4d1113346e1c36
HeavyPunk <kirillkuchma8@gmail.com>
2024-03-17 21:14:51 +0500

    Update

--- web-api/deploy_nginx_conf.sh
+++ web-api/deploy_nginx_conf.sh
@@ -1,5 +1,5 @@
 export ASSETS_PATH=/home/kirieshki/assets
-export MACHINE_IP=158.160.100.38
+export MACHINE_IP=51.250.13.125
 
 envsubst < nginx.template.conf > nginx.conf
 scp -P 22 -i ~/.ssh/ya_cloud nginx.conf kirieshki@$MACHINE_IP:/home/kirieshki/ 
